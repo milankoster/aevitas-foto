3679d97370503f998c64fafca6322eaf2225635c
milankostervangroos <m91989@eon.com>
2025-09-12 21:32:42 +0200

    navbar concept

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
-import { NgClass } from '@angular/common';
+import {NgClass, NgIf} from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, RouterLink, NgIf],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  @Input() transparent = false;
+  @Input() isDimmed = false;
   mobileMenuOpen = false;
 
   toggleMobileMenu() {
