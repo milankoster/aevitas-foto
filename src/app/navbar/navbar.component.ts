068c428cf0bd6fcce90e5230b18307e680b63305
milankostervangroos <m91989@eon.com>
2025-09-13 01:06:34 +0200

    navbar hover

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import {NgClass, NgIf} from '@angular/common';
-import { RouterLink } from '@angular/router';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [NgClass, RouterLink, NgIf],
   templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   @Input() isDimmed = false;
