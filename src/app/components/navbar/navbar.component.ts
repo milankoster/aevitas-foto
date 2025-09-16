1cb034425f1fac9abdfa67f400d068de53096429
milankostervangroos <m91989@eon.com>
2025-09-16 21:15:49 +0200

    mobile menu tweaks

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
-import { NgClass, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import {MobileMenuComponent} from './mobile-menu/mobile-menu.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgClass, NgIf, RouterLink, MobileMenuComponent],
+  imports: [NgIf, RouterLink, MobileMenuComponent],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
@@ -16,9 +16,16 @@ export class NavbarComponent {
 
   toggleMobileMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
+
+    if (this.mobileMenuOpen) {
+      document.body.classList.add('overflow-hidden');
+    } else {
+      document.body.classList.remove('overflow-hidden');
+    }
   }
 
   closeMobileMenu() {
     this.mobileMenuOpen = false;
+    document.body.classList.remove('overflow-hidden');
   }
 }
