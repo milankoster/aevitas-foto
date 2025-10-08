531e2a19e19e84b9e4e33176553c6de48c8a83f1
milankostervangroos <m91989@eon.com>
2025-10-08 17:13:56 +0200

    Improve mini story

--- src/app/components/home/mini-story/mini-story.component.ts
+++ src/app/components/home/mini-story/mini-story.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mini-story',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, NgOptimizedImage],
   templateUrl: './mini-story.component.html',
   styleUrls: ['./mini-story.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
