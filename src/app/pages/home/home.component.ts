a914ae95fe237f0e749f4a6c0772e192b3b5452b
milankostervangroos <m91989@eon.com>
2025-10-25 13:30:36 +0200

    rename folder

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeroComponent } from '../../components/home/hero/hero.component';
 import { IntroComponent } from '../../components/home/intro/intro.component';
 import { MiniGalleryComponent } from '../../components/home/mini-gallery/mini-gallery.component';
-import { BannerComponent } from '../../components/home/mini-story/banner.component';
+import { BannerComponent } from '../../components/home/banner/banner.component';
 
 @Component({
   selector: 'app-home',
