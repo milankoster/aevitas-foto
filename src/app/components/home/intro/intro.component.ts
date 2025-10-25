d29e17dde9419c9d8810042ecb627483e00a92f0
milankostervangroos <m91989@eon.com>
2025-10-25 13:49:10 +0200

    Add button

--- src/app/components/home/intro/intro.component.ts
+++ src/app/components/home/intro/intro.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss',
   standalone: true,
