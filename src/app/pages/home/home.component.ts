03cc0be4ceee1809d840d009d35fc796658c96f2
milankostervangroos <m91989@eon.com>
2025-12-22 12:38:23 +0100

    refactor banner component and banner mobile component into singular banner

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,19 +3,11 @@ import { HeroComponent } from '../../components/home/hero/hero.component';
 import { IntroComponent } from '../../components/home/intro/intro.component';
 import { MiniGalleryComponent } from '../../components/home/mini-gallery/mini-gallery.component';
 import { BannerComponent } from '../../components/home/banner/banner.component';
-import { BannerMobileComponent } from '../../components/home/banner/banner-mobile.component';
 import { FooterComponent } from '../../components/navigation/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    HeroComponent,
-    IntroComponent,
-    MiniGalleryComponent,
-    BannerComponent,
-    BannerMobileComponent,
-    FooterComponent,
-  ],
+  imports: [HeroComponent, IntroComponent, MiniGalleryComponent, BannerComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
